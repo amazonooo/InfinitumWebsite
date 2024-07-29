f5cedc8e07ed2084111f1f2179f3d811253ab92e
amazonooo <garikogannisyan.9@gmail.com>
2024-07-29 21:46:04 +0300

    change padding on footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import FooterNav from '../ui/footer/FooterNav'
 const Footer: FC = () => {
   return (
 		<footer
-			className='max-w-7xl w-full px-5 sm:px-10 xl:px-0 mx-auto overflow-x-clip pt-28 pb-10'
+			className='max-w-7xl w-full px-5 sm:px-10 xl:px-0 mx-auto overflow-x-clip pt-12 pb-10'
 		>
 			<FooterNav />
 		</footer>
