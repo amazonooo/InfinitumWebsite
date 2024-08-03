c55cd7baa00a793eb7676112ef2874d8668212e4
amazonooo <garikogannisyan.9@gmail.com>
2024-08-03 22:08:20 +0300

    change duration on cards

--- src/components/ui/main-info/news/Card.tsx
+++ src/components/ui/main-info/news/Card.tsx
@@ -17,7 +17,7 @@ const Card: FC<ICard> = ({ image, title, desc }) => {
 	return (
 		<Link
 			href={'/'}
-			className={`flex group cursor-pointer flex-col w-[300px] sm:w-[450px] md:w-[350px] lg:w-[425px] xl:w-[550px] rounded-lg border border-white/[0.2] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] bg-[#161616] transition-transform duration-700 hover:shadow-3xl hover:shadow-purple-500/[0.2] ${
+			className={`flex group cursor-pointer flex-col w-[300px] sm:w-[450px] md:w-[350px] lg:w-[425px] xl:w-[550px] rounded-lg border border-white/[0.2] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] bg-[#161616] transition-transform duration-550 hover:shadow-3xl hover:shadow-purple-500/[0.2] ${
 				isHovered ? '-translate-y-2' : 'translate-y-2'
 			}`}
 			onMouseEnter={() => setIsHovered(true)}
@@ -38,10 +38,10 @@ const Card: FC<ICard> = ({ image, title, desc }) => {
 					<p className='text-sm md:text-base'>{desc}</p>
 				</div>
 				<div className='pb-5 pl-2 flex items-center justify-center gap-x-2 cursor-pointer'>
-					<h3 className='group-hover:text-[#cbacf9] transition-all duration-500'>
+					<h3 className='group-hover:text-[#cbacf9] transition-all duration-450'>
 						Читать
 					</h3>
-					<IoExitOutline className='-ml-3.5 opacity-0 group-hover:ml-0 group-hover:opacity-100 transition-all duration-500' />
+					<IoExitOutline className='-ml-3.5 opacity-0 group-hover:ml-0 group-hover:opacity-100 transition-all duration-450' />
 				</div>
 			</div>
 		</Link>
