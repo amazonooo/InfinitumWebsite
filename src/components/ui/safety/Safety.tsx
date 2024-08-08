65e8216660ae05b2d1ceeed6fa25fb943040122d
amazonooo <garikogannisyan.9@gmail.com>
2024-08-08 22:53:39 +0300

    change to white color

--- src/components/ui/safety/Safety.tsx
+++ src/components/ui/safety/Safety.tsx
@@ -42,7 +42,7 @@ const Safety: FC = () => {
 						<h2 className='text-xs sm:text-base md:text-lg whitespace-nowrap'>
 							Выход
 						</h2>
-						<button className='text-base md:text-lg whitespace-nowrap w-max rounded-lg transition-colors duration-300 py-2 px-5 hover:bg-rose-600/70 bg-rose-600 text-black'>
+						<button className='text-base md:text-lg whitespace-nowrap w-max rounded-lg transition-colors duration-300 py-2 px-5 hover:bg-rose-600/70 bg-rose-600 text-white'>
 							Выйти из аккаунта
 						</button>
 					</div>
