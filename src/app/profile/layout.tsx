baf137d27b859f2cf5e8aaddbc23c0c0a6db0327
amazonooo <garikogannisyan.9@gmail.com>
2024-08-09 21:58:23 +0300

    fixed layout file

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,25 +1,14 @@
-import StarsCanvas from '@/components/ui/stars/Stars'
-import { NO_INDEX_PAGE } from '@/constants/seo.constants'
-import { Inter } from 'next/font/google'
 import ProfileLinks from './components/ProfileLinks'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Profile',
-  NO_INDEX_PAGE
-}
+import { ReactNode } from 'react'
 
 export default function ProfileLayout({
-	children,
-	type
+	children
 }: Readonly<{
-	children: React.ReactNode
-	type: string
+	children: ReactNode
 }>) {
   return (
 		<div
-			className={`${inter.className} bg-[#161616] overflow-x-hidden h-full`}
+			className={`bg-[#161616] overflow-x-hidden h-full`}
 		>
 			<ProfileLinks />
 			{children}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,15 +8,6 @@ export const metadata: Metadata = {
 
 export default function ProfilePage() {
   return (
-		<div className='h-full w-full flex flex-col overflow-clip'>
-			<div
-				className='flex flex-col isolate z-[50] items-center justify-center px-5 sm:px-10 md:px-15 lg:px-28 xl:px-[182.5px]'
-				style={{
-					backdropFilter: 'blur(.5px)',
-					transition: '0.45s cubic-bezier(0.6, 0.6, 0, 1) box-shadow',
-				}}
-			>
-			</div>
-		</div>
+		<div></div>
 	)
 }
