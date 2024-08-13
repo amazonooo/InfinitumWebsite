9209ba87e80420411bd6e03a47b6abb649de7d2b
amazonooo <garikogannisyan.9@gmail.com>
2024-08-13 23:12:19 +0300

    change profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+import GeneralMain from './general/components/GeneralMain'
 
 export const metadata: Metadata = {
   title: 'Profile',
@@ -8,6 +9,16 @@ export const metadata: Metadata = {
 
 export default function ProfilePage() {
   return (
-		<div></div>
+		<section className='h-full w-full pt-16'>
+			<div
+				className='flex flex-col isolate z-[50] px-5 sm:px-10 md:px-15 lg:px-28 xl:px-[182.5px]'
+				style={{
+					backdropFilter: 'blur(.5px)',
+					transition: '0.45s cubic-bezier(0.6, 0.6, 0, 1) box-shadow',
+				}}
+			>
+				<GeneralMain />
+			</div>
+		</section>
 	)
 }
