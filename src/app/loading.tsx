f65788c4e68a651213176230f3022d2975a27658
amazonooo <garikogannisyan.9@gmail.com>
2024-08-30 20:01:43 +0300

    loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import styles from './(loader)/loader.module.css'
+
+export default function loading() {
+  return (
+		<div className='h-screen w-screen fixed top-0 left-0 bg-[#161616]'>
+			<div className='w-full h-full flex items-center justify-center'>
+				<div className={styles.loader}></div>
+			</div>
+		</div>
+	)
+}
