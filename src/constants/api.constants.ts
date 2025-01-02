923a330946587955af467cf193fba60e0fdcf0e3
amazonooo <garikogannisyan.9@gmail.com>
2025-01-02 18:03:32 +0300

    change api url

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,5 +1,5 @@
 export const URL = 'https://infinitum.su'
-export const API_URL = `http://localhost:4200`
+export const API_URL = `https://api.infinitum.su`
 export const CDN_URL = 'https://cdn.infinitum.su'
 export const DOMAIN = 'infinitum-website.vercel.app' // На Проде изменить на infinitum.su. SameSite остается Lax
 export const SECURED_REFRESH = true
